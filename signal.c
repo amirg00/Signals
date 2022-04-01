43f92b36526279fd0deacb8a9ed793d18b0a57e8
AmirGill <amirg00@walla.com>
2022-04-01 14:52:47 +0300

    Organized Code

--- signal.c
+++ signal.c
@@ -17,49 +17,51 @@ void sig_handler(int signum)
             printf("I am in sighandler1\n");
             fflush(stdout);
             raise(SIGUSR1);
-            case SIGUSR1:
-                printf("I am in sighandler2\n");
-                fflush(stdout);
-                k=i/j;
-                printf("lalala\n");
-                fflush(stdout);
-                break;
-                case SIGFPE:
-                    printf("I am in sighandler3\n");
-                    fflush(stdout);
-			
-                    int fd[2];
-                    int status;
 
-                    // Set the pipe
-                    if (pipe(fd) < 0)
-                    {
-                        perror("pipe error\n");
-                        exit(1);
-                    }
-			  
-                    // Generate pipe signal
-                    switch (fork())
-                    {
-                        case -1: /* error */
-                        fprintf(stderr, "fork error\n");
-                        exit(1);
-			       
-                        case  0: /* child */
-                        close(fd[1]);
-                        close(fd[0]);  /* closing read end of pipe */
-                        exit(0);
-			 
-                        default: /* parent */
-                        wait(&status);
-                        close(fd[0]);
-                        write(fd[1], "SIGNAL_PIPE\n", 12);
-                    }
+        case SIGUSR1:
+            printf("I am in sighandler2\n");
+            fflush(stdout);
+            k=i/j;
+            printf("lalala\n");
+            fflush(stdout);
+            break;
+
+        case SIGFPE:
+            printf("I am in sighandler3\n");
+            fflush(stdout);
+
+            int fd[2];
+            int status;
+
+            // Set the pipe
+            if (pipe(fd) < 0)
+            {
+                perror("pipe error\n");
+                exit(1);
+            }
+
+            // Generate pipe signal
+            switch (fork())
+            {
+                case -1: /* error */
+                fprintf(stderr, "fork error\n");
+                exit(1);
+
+                case  0: /* child */
+                close(fd[1]);
+                close(fd[0]);  /* closing read end of pipe */
+                exit(0);
+
+                default: /* parent */
+                wait(&status);
+                close(fd[0]);
+                write(fd[1], "SIGNAL_PIPE\n", 12);
+            }
 			
-                    case SIGPIPE:
-                        printf("I am in sighandler4\n");
-                        fflush(stdout);
-                        kill(getpid(), SIGUSR2);
+        case SIGPIPE:
+            printf("I am in sighandler4\n");
+            fflush(stdout);
+            kill(getpid(), SIGUSR2);
     }
 }
 
